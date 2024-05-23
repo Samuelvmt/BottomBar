dc69e5e65e750fc088da7ee05c4457e1487b4001
samuelvmt <samu.7v@gmail.com>
2024-05-23 20:07:11 -0300

    criando um evento de clique nos itens de menu

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -1,14 +1,17 @@
 package br.com.etecia.myapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
-BottomNavigationView bottomNavView;
+BottomNavigationView BottomNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,21 @@ BottomNavigationView bottomNavView;
 
         setContentView(R.layout.activity_main);
         // Apresentando o xml para o java
-        bottomNavView = findViewById(R.id.BottNav);
+        BottomNavView = findViewById(R.id.BottNav);
+
+    //criando um evento de clique nos itens de menu
+       BottomNavView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+           @Override
+           public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+               switch (){
+                   case:;
+                   break;
+
+               }
+
+               return false;
+
+           }
+       });
     }
 }
