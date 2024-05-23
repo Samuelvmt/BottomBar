5674e502c89db8b92d18a82d331e32fb5ba3dfb6
samuelvmt <samu.7v@gmail.com>
2024-05-23 20:15:49 -0300

    ..

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
-BottomNavigationView BottomNavView;
+    BottomNavigationView BottomNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +22,37 @@ BottomNavigationView BottomNavView;
         // Apresentando o xml para o java
         BottomNavView = findViewById(R.id.BottNav);
 
-    //criando um evento de clique nos itens de menu
-       BottomNavView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-           @Override
-           public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-               switch (){
-                   case:;
-                   break;
+        //criando um evento de clique nos itens de menu
+        BottomNavView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.mNews:
+                        Toast.makeText(MainActivity.this,
+                                "Cliquei em News", Toast.LENGTH_SHORT).show();
+                        ;
+                        break;
+                    case R.id.mNews:
+                        Toast.makeText(MainActivity.this,
+                                "Cliquei em Global", Toast.LENGTH_SHORT).show();
+                        ;
+                        break;
+                    case R.id.mNews:
+                        Toast.makeText(MainActivity.this,
+                                "Cliquei em For You", Toast.LENGTH_SHORT).show();
+                        ;
+                        break;
+                    case R.id.mNews:
+                        Toast.makeText(MainActivity.this,
+                                "Cliquei em Trending", Toast.LENGTH_SHORT).show();
+                        ;
+                        break;
 
-               }
+                }
 
-               return false;
+                return false;
 
-           }
-       });
+            }
+        });
     }
 }
