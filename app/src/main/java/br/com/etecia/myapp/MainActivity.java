77ffdc01fddf23e9f24daeb602a0e9f363cb3779
samuelvmt <samu.7v@gmail.com>
2024-05-23 19:58:11 -0300

    apresentando o xml para o java

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+BottomNavigationView bottomNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        // Apresentando o xml para o java
+        bottomNavView = findViewById(R.id.BottNav);
     }
 }
